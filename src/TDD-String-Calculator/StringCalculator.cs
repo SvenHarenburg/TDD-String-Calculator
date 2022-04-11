6dee44457f2d49a9970f40fda2a287cc7df82014
Sven Harenburg <sven@svenharenburg.de>
2022-04-11 18:20:18 +0200

    Moved tests to separate file

--- src/TDD-String-Calculator/StringCalculator.cs
+++ src/TDD-String-Calculator/StringCalculator.cs
@@ -1,5 +1,4 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,24 +11,6 @@ namespace TDD_String_Calculator
         public int Add(string numbers)
         {
             return 0;
-        }
-    }
-
-    public class StringCalculatorTests
-    {
-        [Test]
-        public void Add_Returns_0_For_Empty_String()
-        {
-            // Arrange
-            var input = "";
-            var sut = new StringCalculator();
-
-            // Act
-            var result = sut.Add(input);
-
-            // Assert
-            Assert.That(result, Is.EqualTo(0));
-
         }
     }
 }
---
+++ src/TDD-String-Calculator/StringCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TDD_String_Calculator
+{
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_Returns_0_For_Empty_String()
+        {
+            // Arrange
+            var input = "";
+            var sut = new StringCalculator();
+
+            // Act
+            var result = sut.Add(input);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
